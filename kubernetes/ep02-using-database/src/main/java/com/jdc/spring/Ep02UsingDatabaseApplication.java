089dd3193b274f3176ca070e76b301b704be1e66
Min Lwin <lwin.zawmin@gmail.com>
2025-01-03 23:15:52 +0630

    Share Kubernetes Spring Application using Postgresql DB and volume

---
+++ kubernetes/ep02-using-database/src/main/java/com/jdc/spring/Ep02UsingDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ep02UsingDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ep02UsingDatabaseApplication.class, args);
+	}
+
+}
---
+++ kubernetes/ep02-using-database/src/test/java/com/jdc/spring/Ep02UsingDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ep02UsingDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
